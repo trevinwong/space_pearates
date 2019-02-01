d163f9758812e0bb793a80ee1ef7a959d927de9e
Frances Sin <frances.sin31@gmail.com>
2019-02-01 10:34:06 -0800

    collision component

---
+++ src/collision_component.hpp
@@ -0,0 +1,31 @@
+#ifndef COLLISION_COMPONENT_H
+#define COLLISION_COMPONENT_H
+
+#include "utility.hpp"
+#include "component.hpp"
+#include <vector>
+#include "entity.hpp"
+
+class CollisionComponent : public BaseComponent
+{
+public:
+
+    // Position, size, and rotation of collision box
+	CollisionComponent(glm::vec2 _position, glm::vec2 _size, GLfloat _rotate);
+	~CollisionComponent();
+    
+    bool hasCollided;
+
+    // Entity may collide with multiple objects at once
+    std::vector<Entity> entities;
+
+    // Position, size, and rotation of collision box
+    glm::vec2 position;
+	glm::vec2 size;
+	GLfloat   rotate;
+
+	static const int typeID = TRANSFORM_COMPONENT_TYPEID;
+	inline virtual int getTypeID() const { return typeID; };
+};
+
+#endif
