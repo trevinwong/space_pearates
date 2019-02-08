539c3fd3592cb20cc0cf7c58a02b1a28ba414f26
Hanchen Hou <a3o0b@ugrad.cs.ubc.ca>
2019-02-07 19:42:50 -0800

    Fix color render issue

--- src/EntityFactories/tile_factory.cpp
+++ src/EntityFactories/tile_factory.cpp
@@ -20,7 +20,7 @@ Entity TileFactory::build(int type, glm::vec2 position, glm::vec2 scale)
     colour = new ColorComponent(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
   }
   else if (type == 1) {
-    colour = new ColorComponent(glm::vec4(0.8f, 0.8f, 0.4f, 1.0f));
+    colour = new ColorComponent(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
   }
 
   Entity e;
