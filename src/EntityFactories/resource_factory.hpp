36a27b64ece38670b802e4c01d71d7eb250486bf
Frances Sin <frances.sin31@gmail.com>
2019-02-19 11:35:14 -0800

    commit resource factory class

---
+++ src/EntityFactories/resource_factory.hpp
@@ -0,0 +1,19 @@
+#ifndef RESOURCE_FACTORY_H
+#define RESOURCE_FACTORY_H
+
+#include "entity.hpp"
+#include "program.hpp"
+#include "Components/sprite_component.hpp"
+#include "Components/transform_component.hpp"
+#include "Components/color_component.hpp"
+#include "Components/resource_component.hpp"
+
+class ResourceFactory
+{
+    public:
+    ResourceFactory();
+    ~ResourceFactory();
+    Entity build(glm::vec2 position, glm::vec2 scale);
+};
+
+#endif
