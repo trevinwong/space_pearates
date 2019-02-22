fb29a6172b35d015c740d83570ebbab86ebc41e3
Trevin Wong <d3s0b@ugrad.cs.ubc.ca>
2019-02-21 17:43:21 -0800

    Remove event systems in favor of components as events.

--- src/Systems/event_system.cpp
+++
@@ -1,9 +0,0 @@
-#include "event_system.hpp"
-
-void EventSystem::propagateEvent(Event *event)
-{
-  for (Subscriber *subscriber : subscribers[event->getTypeId()]) {
-    subscriber->processEvent(event);
-  }
-  delete event;
-}
